6afe93aa05c3aa07e313deabb9cb8451eabf0cfe
Towerbank <59486210+Towerbank@users.noreply.github.com>
2021-03-18 16:50:33 +0300

    проба слайдера 18.03

--- gulpfile.js
+++ gulpfile.js
@@ -23,21 +23,25 @@ var path = {
   src: {
     html: 'src/*.html',
     styles: 'src/styles/*.scss',
+    js: 'src/*.js',
     img: 'src/img/*.{jpg,jpeg,png,webp,svg}',
     svg: "src/img/svg/*.svg"
     },
   build: {
     html: 'build/',
     styles: 'build/css/',
+    js: 'build/',
     img: 'build/img/',
     svg: "build/img/svg"
     },
   watch: {
     html: 'src/**/*.html',
     styles: 'src/styles/**/*.scss',
+    js: 'src/**/*.js',
     img: 'src/img/**/*.{jpg,jpeg,png,webp,svg}',
     svg: "src/img/svg/*.svg"
     },
+    
   base: './build'
 };
 
@@ -68,6 +72,13 @@ function styles() {
   .src(path.src.styles)
   .pipe(sass().on('error', sass.logError))
   .pipe(gulp.dest(path.build.styles))
+  .pipe(reload({stream: true}));
+};
+
+function js() {
+  return gulp
+  .src(path.src.js)
+  .pipe(gulp.dest(path.build.js))
   .pipe(reload({stream: true}));
 };
 
@@ -115,14 +126,16 @@ function svg() {
 function watchFiles() {
   gulp.watch([path.watch.html], html);
   gulp.watch([path.watch.styles], styles);
+  gulp.watch([path.watch.js], js);
   gulp.watch([path.watch.img], img);
   gulp.watch([path.watch.svg], svg);
 };
 
 gulp.task('html', html);
 gulp.task('styles', styles);
+gulp.task('js', js);
 gulp.task('img', img);
 gulp.task('svg', svg);
 
-gulp.task('build', gulp.series(clean, gulp.parallel(html, styles, img, svg)));
+gulp.task('build', gulp.series(clean, gulp.parallel(html, styles, js, img, svg)));
 gulp.task('watch', gulp.parallel(watchFiles, browserSync));
---
+++ src/script.js
@@ -0,0 +1,51 @@
+
+  let position = 0;
+  const slidesToShow = 1;
+  const slidesToScroll = 1;
+  const container = document.querySelector('.slider');
+  const track = document.querySelector(".slider__track");
+  // const item = document.querySelector(".slider__item");
+  const btnLeft = document.querySelector(".btn__left");
+  const btnRight = document.querySelector(".btn__right");
+  const items = document.querySelectorAll(".slider__item");
+  const itemsCount = items.length;
+  const itemWidth = container.clientWidth / slidesToShow;
+  const movePosition = slidesToScroll * itemWidth;
+
+items.forEach((item) => {
+  item.style.minWidth = `${itemWidth}px`;
+});
+
+
+  btnRight.addEventListener('click', () => {
+    const itemsLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
+    console.log('ponk')
+    position -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth ;
+    setPosition();
+    checkBtns();
+  });
+
+  btnLeft.addEventListener('click', () => {
+    const itemsLeft = Math.abs(position) / itemWidth;
+    console.log('ponk')
+    position += itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth ;
+    setPosition();
+    checkBtns();
+  });
+
+
+const setPosition = () => {
+  track.style.transform = `translateX(${position}px)`;
+  };
+
+const checkBtns = () => {
+  btnLeft.disabled = position === 0
+  btnRight.disabled = position <= -(itemsCount - slidesToShow) * itemWidth;
+
+  };
+
+  checkBtns();
+
+
+
+
